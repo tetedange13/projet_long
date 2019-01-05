2b6779ec91ebbf21a1b001d2a43cfa1a8a98d19e
tetedange13 <felix.deslacs@gmail.com>
2019-01-05 17:21:41 +0100

    Added the 40 pdbs (domains instead) constituing the RIPC dataset

--- bench.py
+++ bench.py
@@ -19,24 +19,39 @@ import os
 import urllib.request as urlreq
 import pandas as pd
 import pyquery as pyq
+import re
 
 
 class AppURLopener(urlreq.FancyURLopener):
     version = "Mozilla/5.0"
 
 
-def dl_pdb_2(pdb_id):
+def get_url_dom(sid_dom):
     """Download the given files from the webpage of a folder"""
+    opener = AppURLopener()
+    url_to_open = "http://scop.berkeley.edu/sid=" + sid_dom
 
-    url_pdb = "https://files.rcsb.org/download/" + pdb_id + ".pdb"
+    with opener.open(url_to_open) as response:
+        pq = pyq.PyQuery(response.read())
 
-    print("Dowloading " + pdb_id + ".pdb from the RCSB website...")
-    urlreq.urlretrieve(url_pdb, "data/" + pdb_id + '.pdb')
-    print("Download finished !\n")
+    list_hrefs = []
+    for a in pq.find('a'):
+        current_href = a.get('href')
+        if 'pdbstyle' in current_href and sid_dom in current_href:
+            list_hrefs.append(current_href)
+
+    if len(list_hrefs) != 1:
+        print("ERROR: Several urls possible !! ")
+        sys.error(2)
+    else:
+        return list_hrefs[0]
 
 
-def dl_pdb(sid_dom, pdb_id):
-    """Download the given files from the webpage of a folder"""
+
+def dl_pdb_2(sid_dom, pdb_id):
+    """
+    Download the given files from the webpage of a folder
+    """
 
     url_pdb = ("http://scop.berkeley.edu/astral/pdbstyle/ver=2.05&id=" +
                sid_dom + "&output=txt")
@@ -46,20 +61,16 @@ def dl_pdb(sid_dom, pdb_id):
     print("Download finished !\n")
 
 
-def get_domain_range(sid_dom):
-    opener = AppURLopener()
+def dl_pdb(url_dom, pdb_id):
+    """
+    Download the given files from the webpage of a folder
+    """
+    good_url = re.sub(r'(output=html)', 'output=txt', url_dom)
 
-    my_url = "http://scop.berkeley.edu/sid=" + sid_dom
-
-    with opener.open(my_url) as response:
-        pq = pyq.PyQuery(response.read())
-
-    titre = pq.find('title').text()
-    print(titre)
-    sys.exit()
-    range_dom = titre.split(':')[-1]
-    # return list(map(int, range_dom.spl))
-    return range_dom
+    print("Dowloading the good domain of " + pdb_id + ".pdb from the SCOP " +
+          "website...")
+    urlreq.urlretrieve(good_url, "data/" + pdb_id + '.pdb')
+    print("Download finished !\n")
 
 
 
@@ -78,24 +89,30 @@ if __name__ == "__main__":
         pdb_id_dom1, pdb_id_dom2 = dom1_sid[1:5], dom2_sid[1:5]
         chainID_sid1, chainID_sid2 = dom1_sid[5], dom2_sid[5]
 
-        if idx == 1:
-            print("salut")
-            # Deals with the case where chainID is not specified ('_'):
-            if chainID_sid1 == '_':
-                chainID_dom1 = 'A'
-            else:
-                chainID_dom1 = chainID_sid1.upper()
+        print(dom1_sid, dom2_sid)
 
-            if chainID_sid2 == '_':
-                chainID_dom2 = 'A'
-            else:
-                chainID_dom2 = chainID_sid2.upper()
+
+        # if idx == 1:
+        if True:
+            # print("salut")
+            # # Deals with the case where chainID is not specified ('_'):
+            # if chainID_sid1 == '_':
+            #     chainID_dom1 = 'A'
+            # else:
+            #     chainID_dom1 = chainID_sid1.upper()
+            #
+            # if chainID_sid2 == '_':
+            #     chainID_dom2 = 'A'
+            # else:
+            #     chainID_dom2 = chainID_sid2.upper()
 
             # If the PDB is absent from the data/ folder, it is downloaded:
             if not os.path.isfile("data/" + pdb_id_dom1 + '.pdb'):
-                dl_pdb(dom1_sid, pdb_id_dom1)
+                url_dom1 = get_url_dom(dom1_sid)
+                dl_pdb(url_dom1, pdb_id_dom1)
             if not os.path.isfile("data/" + pdb_id_dom2 + '.pdb'):
-                dl_pdb(dom2_sid, pdb_id_dom2)
+                url_dom2 = get_url_dom(dom2_sid)
+                dl_pdb(url_dom2, pdb_id_dom2)
         #
         #     print(pdb_id_dom1+chainID_dom1, pdb_id_dom2+chainID_dom2)
         #     os.system("./main.py -p data/" + pdb_id_dom1 + '.pdb -r data/' +
