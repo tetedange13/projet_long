d5e08dca34ccfe60fe70e7983f290f632ee14368
tetedange13 <felix.deslacs@gmail.com>
2019-01-05 16:44:27 +0100

    Tried to use instead SCOP special PDB, with domain only

--- bench.py
+++ bench.py
@@ -25,11 +25,22 @@ class AppURLopener(urlreq.FancyURLopener):
     version = "Mozilla/5.0"
 
 
-def dl_pdb(pdb_id):
+def dl_pdb_2(pdb_id):
     """Download the given files from the webpage of a folder"""
 
     url_pdb = "https://files.rcsb.org/download/" + pdb_id + ".pdb"
 
+    print("Dowloading " + pdb_id + ".pdb from the RCSB website...")
+    urlreq.urlretrieve(url_pdb, "data/" + pdb_id + '.pdb')
+    print("Download finished !\n")
+
+
+def dl_pdb(sid_dom, pdb_id):
+    """Download the given files from the webpage of a folder"""
+
+    url_pdb = ("http://scop.berkeley.edu/astral/pdbstyle/ver=2.05&id=" +
+               sid_dom + "&output=txt")
+    print(url_pdb)
     print("Dowloading " + pdb_id + ".pdb from the RCSB website...")
     urlreq.urlretrieve(url_pdb, "data/" + pdb_id + '.pdb')
     print("Download finished !\n")
@@ -67,28 +78,24 @@ if __name__ == "__main__":
         pdb_id_dom1, pdb_id_dom2 = dom1_sid[1:5], dom2_sid[1:5]
         chainID_sid1, chainID_sid2 = dom1_sid[5], dom2_sid[5]
 
-        dom_range1 = get_domain_range(dom1_sid)
-        dom_range2 = get_domain_range(dom2_sid)
-        print(dom_range1, dom_range2)
+        if idx == 1:
+            print("salut")
+            # Deals with the case where chainID is not specified ('_'):
+            if chainID_sid1 == '_':
+                chainID_dom1 = 'A'
+            else:
+                chainID_dom1 = chainID_sid1.upper()
 
-        # if idx == 1:
-        #     print("salut")
-        #     # Deals with the case where chainID is not specified ('_'):
-        #     if chainID_sid1 == '_':
-        #         chainID_dom1 = 'A'
-        #     else:
-        #         chainID_dom1 = chainID_sid1.upper()
-        #
-        #     if chainID_sid2 == '_':
-        #         chainID_dom2 = 'A'
-        #     else:
-        #         chainID_dom2 = chainID_sid2.upper()
-        #
-        #     # If the PDB is absent from the data/ folder, it is downloaded:
-        #     if not os.path.isfile("data/" + pdb_id_dom1 + '.pdb'):
-        #         dl_pdb(pdb_id_dom1)
-        #     if not os.path.isfile("data/" + pdb_id_dom2 + '.pdb'):
-        #         dl_pdb(pdb_id_dom2)
+            if chainID_sid2 == '_':
+                chainID_dom2 = 'A'
+            else:
+                chainID_dom2 = chainID_sid2.upper()
+
+            # If the PDB is absent from the data/ folder, it is downloaded:
+            if not os.path.isfile("data/" + pdb_id_dom1 + '.pdb'):
+                dl_pdb(dom1_sid, pdb_id_dom1)
+            if not os.path.isfile("data/" + pdb_id_dom2 + '.pdb'):
+                dl_pdb(dom2_sid, pdb_id_dom2)
         #
         #     print(pdb_id_dom1+chainID_dom1, pdb_id_dom2+chainID_dom2)
         #     os.system("./main.py -p data/" + pdb_id_dom1 + '.pdb -r data/' +
